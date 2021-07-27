93a388e1ab258b36bb7d2199f876d9e97515871e
Newman <Jonathan.Newman@henryschein.com>
2021-07-27 09:28:27 -0600

    Init commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('postman-request')
+
+const geocode = (address, callback) => {
+    const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + encodeURIComponent(address) + ".json?access_token=pk.eyJ1Ijoiam5ld21hbjEwMCIsImEiOiJja3E4anVicXowMnV3Mm9xbXQ2dnRsdXJjIn0.cgHNPMpDOZOrcqxQ2RBIBA&limit=1"
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (!body.features.length) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
